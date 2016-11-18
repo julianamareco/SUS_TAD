bdcf6951bbff76070c35732d591c2b6f9a1fde26
Juliana Mareco <Juliana Mareco@10.2.14.221>
2016-11-18 12:23:36 -0200

    Primeiro Commit

---
+++ SUS_TAD/src/sus_tad/Atendimento.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sus_tad;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Juliana Mareco
+ */
+public class Atendimento {
+    Scanner entrada = new Scanner (System.in);
+    public void Menu(){
+        int opcao;
+        System.out.print("1. Atendimento Normal");
+        System.out.print("2. Atendimento Preferêncial");
+        opcao = entrada.nextInt();
+        
+        
+        
+    }
+}
